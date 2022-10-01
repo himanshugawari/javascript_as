1644c8797f48a7b2079b6b54a9ff8cef3f521957
himanshugawari <himanshu25031991@gmail.com>
2022-10-01 23:14:56 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hello!');
+let arr = ['Himanshu', 'Atsuko'];
+
+let obj = {
+  name: 'Himanshu',
+  city: 'Mumbai',
+  getIntro: function () {
+    return `${this.name} from ${this.city}`;
+  },
+};
+
+let obj2 = {
+  name: 'Atsuko',
+};
+
+obj2.__proto__ = obj;
+
+console.log(obj2.name);
+console.log(obj2.city);
+console.log(obj.getIntro());
+console.log(obj2.getIntro());
+
+//  prtotype chain
+// null > Object > Array
+
+// arr.__proto__ === Array.prototype
+// arr.__proto__.__proto__ === Object.prototype
+// arr.__proto__.__proto__.__proto__ === null
+
+// null > Object > Function
