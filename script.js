8452535bbe34cbe340b1994f08689b881062419f
himanshugawari <himanshu25031991@gmail.com>
2022-10-01 20:42:22 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,94 +1,212 @@
-let arr = [1, 2, 4, 8, 10];
-console.log(arr);
+// Promises in JS
 
-console.log('------------------ map ---------------------------');
+// synchronous vs assynchronous code
 
-// map method
-let tempMap = arr.map((value, index, array) => {
-  return value * 2;
+// console.log('--------------------synchronous----------------------');
+// console.log('start');
+// console.log('sync code');
+// console.log('stop');
+
+// console.log('--------------------asynchronous----------------------');
+// console.log('start');
+// setTimeout(() => {
+//   console.log('async code');
+// }, 0);
+// console.log('stop');
+
+// console.log('--------------------Promise----------------------');
+// console.log('start');
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let result = true;
+//     if (result) {
+//       resolve('RESOLVED');
+//     } else {
+//       reject('REJECTED');
+//     }
+//   }, 0);
+// });
+// p.then((result) => {
+//   console.log(result);
+// }).catch((error) => {
+//   console.log(error);
+// });
+// console.log('stop');
+
+console.log('--------------------Promises----------------------');
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('p1'), 1000);
 });
-console.log(tempMap);
-
-// polyfill for map
-Array.prototype.myMap = function (callback) {
-  let result = [];
-  for (let i = 0; i < this.length; i++) {
-    result.push(callback(this[i], i, this));
-  }
-  return result;
+const p2 = function temp(val) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`p2 ${val}`), 1000);
+  });
 };
 
-let tempMyMap = arr.myMap((value, index, array) => {
-  return value * 2;
-});
-console.log(tempMyMap);
+// console.log('--------------------promises chaining----------------------');
+// p1.then((res) => {
+//   console.log(res);
+//   return p2('helloooo world');
+// }).then((res) => {
+//   console.log(res);
+// });
 
-console.log('------------------ reduce ---------------------------');
+// console.log('--------------------promises combinator all----------------------');
+// wait till all promises are completed and then return
+// even if one promise fails all fails and returns in catch failed promise
 
-// reduce method
-let tempReduce = arr.reduce(
-  (previousValue, currentValue, currentIndex, array) => {
-    return previousValue + currentValue;
-  },
-  5
-);
-console.log(tempReduce);
+// Promise.all([p1, p2('hello')])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
-// polyfill for reduce
-Array.prototype.myReduce = function (callback, initailValue) {
-  let result = initailValue;
-  for (let i = 0; i < this.length; i++) {
-    result = callback(result, this[i], i, this);
-  }
-  return result;
+// console.log('--------------------promises combinator race-------------------');
+// returns the first promise to be fullfilled or rejected
+
+// Promise.race([p1, p2('hello')])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log('---------------promises combinator allSettled----------------');
+// wait till all are fullfilled or rejected and then return arry with all fullfilled or rejected promises
+
+// Promise.allSettled([p1, p2('hello')])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log('---------------promises combinator any----------------');
+// returns the first promise to be fullfilled, if any are rejected prmomises are ignored  before the first fulfilled promise
+// if all promises fail, it fails
+
+// Promise.any([p1, p2('hello')])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log('---------------async/await----------------');
+// const result = async () => {
+//   try {
+//     const m1 = await p1;
+//     const m2 = await p2('async-await');
+//     console.log(m1, m2);
+//   } catch (error) {
+//     console.log('error', error);
+//   }
+// };
+// result();
+
+// console.log('---------------promise polyfill----------------');
+// const tempPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(2);
+//   }, 1000);
+// });
+
+// tempPromise.then((res) => console.log(res)).catch((err) => console.log(err));
+
+// function MyPromise(executor) {
+//   let onResolve,
+//     onReject,
+//     isFullfilled = false,
+//     isRejected = false,
+//     isCalled = false,
+//     value;
+//   function resolve(val) {
+//     isFullfilled = true;
+//     value = val;
+//     if (typeof onResolve === 'function') {
+//       onResolve(val);
+//       isCalled = true;
+//     }
+//   }
+//   function reject(val) {
+//     isRejected = true;
+//     value = val;
+//     if (typeof onReject === 'function') {
+//       onReject(val);
+//       isCalled = true;
+//     }
+//   }
+//   this.then = function (callbcak) {
+//     onResolve = callbcak;
+//     if (isFullfilled && !isCalled) {
+//       isCalled = true;
+//       onResolve(value);
+//     }
+//     return this;
+//   };
+//   this.catch = function (callbcak) {
+//     onReject = callbcak;
+//     if (isRejected && !isCalled) {
+//       isCalled = true;
+//       onReject(value);
+//     }
+//     return this;
+//   };
+//   try {
+//     executor(resolve, reject);
+//   } catch (error) {
+//     reject(error);
+//   }
+// }
+
+// const tempMyPromise = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(2);
+//   }, 1000);
+// });
+
+// tempMyPromise.then((res) => console.log(res)).catch((err) => console.log(err));
+
+// MyPromise.resolve = (val) =>
+//   new MyPromise(function executor(resolve, reject) {
+//     resolve(val);
+//   });
+
+// MyPromise.reject = (val) =>
+//   new MyPromise(function executor(resolve, reject) {
+//     reject(val);
+//   });
+
+// MyPromise.resolve(3).then((res) => console.log(res));
+
+console.log('---------------promise.all polyfill----------------');
+// Promise.all([p1, p2('hello')]).then((res) => console.log(res));
+
+Promise.myAll = (promises) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+
+    if (!promises.length) {
+      resolve(results);
+      return;
+    }
+
+    let pending = promises.length;
+
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise).then((res) => {
+        results[index] = res;
+        pending--;
+        if (pending === 0) resolve(results);
+      }, reject);
+    });
+  });
 };
-
-let tempMyReduce = arr.myReduce(
-  (previousValue, currentValue, currentIndex, array) => {
-    return previousValue + currentValue;
-  },
-  5
-);
-console.log(tempMyReduce);
-
-console.log('------------------ filter ---------------------------');
-
-// filter method
-let tempFilter = arr.filter((value, index, array) => {
-  return value > 5;
-});
-console.log(tempFilter);
-
-// polyfill for filter
-Array.prototype.myFilter = function (callback) {
-  let result = [];
-  for (let i = 0; i < this.length; i++) {
-    if (callback(this[i], i, this)) result.push(this[i]);
-  }
-  return result;
-};
-
-let tempMyFilter = arr.myFilter((value, index, array) => {
-  return value > 5;
-});
-console.log(tempMyFilter);
-
-console.log('------------------ find ---------------------------');
-
-// find method
-let tempFind = arr.find((value, index, array) => {
-  return value === 8;
-});
-console.log(tempFind);
-
-// polyfill for find
-Array.prototype.myFind = function (callback) {
-  for (let i = 0; i < this.length; i++) {
-    if (callback(this[i], i, this)) return this[i];
-  }
-};
-
-let tempMyFind = arr.myFind((value, index, array) => {
-  return value === 8;
-});
-console.log(tempMyFind);
+Promise.myAll([p1, p2('hello')])
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
